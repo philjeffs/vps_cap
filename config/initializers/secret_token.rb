11101320a49870fbdc3c6fc7182538c270a5d1be
Phil Jeffs <philipjeffs@gmail.com>
2013-02-23 17:24:46 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VpsCap::Application.config.secret_token = '25bf16aaa916efade32431765d73186e304331fb5056878b619de9dbcfa4391c9183c92e5cef0d82c3c0ee5cd7afd2ee2b2986b29cb51987b8189997e2186712'
